98b51704f5bd49482ae9a7b61d2b8aed9ec7b5a5
Тима <tima.team.ru@gmail.com>
2024-01-26 09:18:49 +0300

    польская нотация

--- CalcultrApp/CalcultrApp.cpp
+++ CalcultrApp/CalcultrApp.cpp
@@ -20,7 +20,7 @@ int main()
     for (int i = 0; i < size; ++i) {
         cout << "[ " << arr[i] << " ] ";
     }
-
+    cout << endl << polish_natation(arr, size, st);
 
 
     /*try
--- CalcultrApp/functions.h
+++ CalcultrApp/functions.h
@@ -3,6 +3,12 @@
 #include <iostream>
 using namespace std;
 
+void delete_spaces(string& input);
+void scan(string& input);
+void string_to_array(string& input, string arr[], int& size);
+string polish_natation(string* arr, int& size, stack& st);
+int type(string x);
+
 void delete_spaces(string &input) {
 	string new_string = "";
 	for (int i = 0; input[i] != '\0'; ++i) {
@@ -27,9 +33,8 @@ void string_to_array(string& input, string arr[], int& size) {
 			arr[size] = x;
 			++size;
 			x = "";
-			x += input[i];
 		}
-		else if (input[i] == '+' || input[i] == '-' || input[i] == '(' || input[i] == ')' || input[i] == '*' || input[i] == '/' || input[i] == '^') {
+		if (input[i] == '+' || input[i] == '-' || input[i] == '(' || input[i] == ')' || input[i] == '*' || input[i] == '/' || input[i] == '^' || input[i] == '!') {
 			if (x != "") {
 				arr[size] = x;
 				++size;
@@ -43,4 +48,32 @@ void string_to_array(string& input, string arr[], int& size) {
 			x += input[i];
 		}
 	}
+	if (x != "") {
+		arr[size] += x;
+		++size;
+	}
+}
+
+string polish_natation(string*arr, int& size, stack& st) {
+	string ans = "";
+	for (int i = 0; i < size; ++i) {
+		if (type(arr[i]) == 0) {
+			ans += arr[i] + " ";
+		}
+		else if (type(arr[i]) == 1) {
+
+		}
+	}
+	return ans;
+}
+int type(string x) {
+	if (x == "+" || x == "-" || x == "/" || x == "*" || x == "!" || x == "^")
+		return 1;
+	if (x == "(")
+		return 2;
+	if (x == ")")
+		return 3;
+	if (x == "sin" || x == "cos" || x == "tg" || x == "ctg" || x == "arcsin" || x == "arccos" || x == "arctg" || x == "arcctg")
+		return 4;
+	return 0;
 }
